46a069863d6dad2dc7af51398853fb67048559d8
curry-pan <hibiki.19n1102209@gmail.com>
2024-02-11 01:39:00 +0900

    a

--- thank.php
+++ thank.php
@@ -1,11 +1,2 @@
 <p>ありがとうございます。</p>
 <button class="checkout-cta" onclick="location.href='product.php'">買物を続ける</button>
-<?php
-session_start();
-if(isset($_REQUEST["cardNum"])){
-    $pdo=new PDO('mysql:host=localhost;dbname=ec;charset=utf8','root', '');
-    $sql=$pdo->prepare('insert into pass values(?,?,?)');
-    $sql->execute([$_SESSION['customer']['id'],$_REQUEST['cardNum'], $_REQUEST['pass']]);
-    $_SESSION['product'] = null;
-}
-?>
